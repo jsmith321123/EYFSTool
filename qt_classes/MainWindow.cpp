f10bed8069fb72bd1c70dd2b7ca38dab084092cb
jsmith321123 <joshuaellissmith26@gmail.com>
2017-02-11 14:09:55 +0000

    Update MainWindow.cpp

--- qt_classes/MainWindow.cpp
+++ qt_classes/MainWindow.cpp
@@ -40,7 +40,7 @@ void NavigationBar::setAddChild() {
 
 void NavigationBar::setViewChild() {
 	mw_->changeScreen(1);
-	static_cast<ViewChildScreen*>(mw_->screens[1])->updateList();
+	mw_->screens[1]->updateList();
 }
 
 void NavigationBar::setCreateUser() {
